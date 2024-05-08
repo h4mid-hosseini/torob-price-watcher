9d38b165af084254e3ddd92fee83a7464cfc31e3
Hamid Hosseini <info.husseini@gmail.com>
2024-05-08 11:19:32 +0330

    action taken status added to detemine status of dog barks!

--- TorobWatcher/watcher/models.py
+++ TorobWatcher/watcher/models.py
@@ -20,6 +20,7 @@ class WatchDogBark(models.Model):
     title = models.CharField(max_length=200)
     product = models.ForeignKey(WatchingProducts, on_delete=models.CASCADE, related_name='barks')
     price = models.PositiveBigIntegerField()
-    
+    action_taken = models.BooleanField(default=False)
+
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
